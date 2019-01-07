40f8cc1808cd5dd4ff266097a39d1e98dbd41849
Julio Oliveira <julio.oliveira@mycatwalk.com>
2019-01-07 17:15:07 -0200

    started layout component

---
+++ src/components/Layouts/Layout.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import classes from './Layout.css';
+
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
+    sideDrawerOpenedHandler = () =>{
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+
+    render(){
+        return(
+            <Aux>
+                <Toolbar toggle={this.sideDrawerOpenedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+
+export default Layout;
