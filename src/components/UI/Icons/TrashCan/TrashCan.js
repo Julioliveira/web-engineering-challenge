efbe6bfc5734b8864a6bd8fdaf78a58fe8e4e255
Julio Oliveira <julioclub96@gmail.com>
2019-01-20 13:52:24 -0200

    added modal and search changes

--- src/components/UI/Icons/TrashCan/TrashCan.js
+++ src/components/UI/Icons/TrashCan/TrashCan.js
@@ -4,17 +4,19 @@ import classes from './TrashCan.css'
 const trashIcon = (props) => {
 
     let trashClasses = [classes.trashDiv]
-    if(props.spin){
+    if (props.spin) {
         trashClasses.push(classes.spin)
     }
-    return (<div className={trashClasses.join(' ')}>
-        <div className={classes.trashLid}> </div>
-        <div className={classes.can} >
-            <div className={classes.trashLine1} ></div>
-            <div className={classes.trashLine2} ></div>
-            <div className={classes.trashLine3} ></div>
+    return (
+        <div className={trashClasses.join(' ')}>
+            <div className={classes.trashLid}> </div>
+            <div className={classes.can} >
+                <div className={classes.trashLine1} ></div>
+                <div className={classes.trashLine2} ></div>
+                <div className={classes.trashLine3} ></div>
+            </div>
         </div>
-    </div>)
+    )
 }
 
 export default trashIcon
